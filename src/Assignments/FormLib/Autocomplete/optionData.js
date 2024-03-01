0d99a9d042c562a2e90e50fca408bae8ce801fd8
joshirakesh165 <joshirakesh165@gmail.com>
2024-03-01 14:50:40 +0530

    Code pushed from Api

---
+++ src/Assignments/FormLib/Autocomplete/optionData.js
@@ -0,0 +1,322 @@
+export default  [
+    {
+        "label": "Nedra Fritsch",
+        "value": "Nedra Fritsch"
+    },
+    {
+        "label": "Jayce Toy",
+        "value": "Jayce Toy"
+    },
+    {
+        "label": "Collin Pfannerstill III",
+        "value": "Collin Pfannerstill III"
+    },
+    {
+        "label": "Corrine Kirlin",
+        "value": "Corrine Kirlin"
+    },
+    {
+        "label": "Ena Lebsack",
+        "value": "Ena Lebsack"
+    },
+    {
+        "label": "Sasha McLaughlin",
+        "value": "Sasha McLaughlin"
+    },
+    {
+        "label": "Fausto Koch",
+        "value": "Fausto Koch"
+    },
+    {
+        "label": "Willa Kiehn",
+        "value": "Willa Kiehn"
+    },
+    {
+        "label": "Gussie Rodriguez",
+        "value": "Gussie Rodriguez"
+    },
+    {
+        "label": "Jon Prohaska",
+        "value": "Jon Prohaska"
+    },
+    {
+        "label": "Bailee Hand",
+        "value": "Bailee Hand"
+    },
+    {
+        "label": "Cristobal Bernier",
+        "value": "Cristobal Bernier"
+    },
+    {
+        "label": "Letha Nienow",
+        "value": "Letha Nienow"
+    },
+    {
+        "label": "Conner Lowe",
+        "value": "Conner Lowe"
+    },
+    {
+        "label": "Cloyd Raynor",
+        "value": "Cloyd Raynor"
+    },
+    {
+        "label": "Eveline Abbott",
+        "value": "Eveline Abbott"
+    },
+    {
+        "label": "Darwin Rolfson",
+        "value": "Darwin Rolfson"
+    },
+    {
+        "label": "Paul Anderson",
+        "value": "Paul Anderson"
+    },
+    {
+        "label": "Reina Graham",
+        "value": "Reina Graham"
+    },
+    {
+        "label": "Dr. Natalia Kunde",
+        "value": "Dr. Natalia Kunde"
+    },
+    {
+        "label": "Jerome Raynor",
+        "value": "Jerome Raynor"
+    },
+    {
+        "label": "Della Dickens",
+        "value": "Della Dickens"
+    },
+    {
+        "label": "Joyce Gleason",
+        "value": "Joyce Gleason"
+    },
+    {
+        "label": "Stephany Bayer",
+        "value": "Stephany Bayer"
+    },
+    {
+        "label": "Tavares Watsica",
+        "value": "Tavares Watsica"
+    },
+    {
+        "label": "Imelda Swift",
+        "value": "Imelda Swift"
+    },
+    {
+        "label": "Wilhelmine Deckow DVM",
+        "value": "Wilhelmine Deckow DVM"
+    },
+    {
+        "label": "Sabrina Dare",
+        "value": "Sabrina Dare"
+    },
+    {
+        "label": "Hildegard Langworth",
+        "value": "Hildegard Langworth"
+    },
+    {
+        "label": "Trevion Dooley",
+        "value": "Trevion Dooley"
+    },
+    {
+        "label": "Norwood Lind",
+        "value": "Norwood Lind"
+    },
+    {
+        "label": "Holly Jacobs",
+        "value": "Holly Jacobs"
+    },
+    {
+        "label": "Laverne Hammes",
+        "value": "Laverne Hammes"
+    },
+    {
+        "label": "Kayla Lebsack",
+        "value": "Kayla Lebsack"
+    },
+    {
+        "label": "Devante Yost",
+        "value": "Devante Yost"
+    },
+    {
+        "label": "Maymie Brekke DVM",
+        "value": "Maymie Brekke DVM"
+    },
+    {
+        "label": "Jack Lakin",
+        "value": "Jack Lakin"
+    },
+    {
+        "label": "Sigrid Collins",
+        "value": "Sigrid Collins"
+    },
+    {
+        "label": "Giovanni McCullough",
+        "value": "Giovanni McCullough"
+    },
+    {
+        "label": "Evangeline Schultz",
+        "value": "Evangeline Schultz"
+    },
+    {
+        "label": "London Johnson",
+        "value": "London Johnson"
+    },
+    {
+        "label": "Ms. Aurore Bernhard",
+        "value": "Ms. Aurore Bernhard"
+    },
+    {
+        "label": "Rico Conn MD",
+        "value": "Rico Conn MD"
+    },
+    {
+        "label": "Lucious Gulgowski",
+        "value": "Lucious Gulgowski"
+    },
+    {
+        "label": "Elwin Reilly",
+        "value": "Elwin Reilly"
+    },
+    {
+        "label": "Tomasa Leannon",
+        "value": "Tomasa Leannon"
+    },
+    {
+        "label": "Eloy Howe",
+        "value": "Eloy Howe"
+    },
+    {
+        "label": "Bo Fritsch",
+        "value": "Bo Fritsch"
+    },
+    {
+        "label": "Duane Lind",
+        "value": "Duane Lind"
+    },
+    {
+        "label": "Richmond Mitchell",
+        "value": "Richmond Mitchell"
+    },
+    {
+        "label": "Missouri Weissnat",
+        "value": "Missouri Weissnat"
+    },
+    {
+        "label": "Elias Hackett",
+        "value": "Elias Hackett"
+    },
+    {
+        "label": "Rex Quigley V",
+        "value": "Rex Quigley V"
+    },
+    {
+        "label": "Sidney Kuphal",
+        "value": "Sidney Kuphal"
+    },
+    {
+        "label": "Lonnie Emard",
+        "value": "Lonnie Emard"
+    },
+    {
+        "label": "Maximillian Stehr V",
+        "value": "Maximillian Stehr V"
+    },
+    {
+        "label": "Kendall Ullrich",
+        "value": "Kendall Ullrich"
+    },
+    {
+        "label": "Adeline Hintz",
+        "value": "Adeline Hintz"
+    },
+    {
+        "label": "Jody Streich DDS",
+        "value": "Jody Streich DDS"
+    },
+    {
+        "label": "Ms. Pete Cole",
+        "value": "Ms. Pete Cole"
+    },
+    {
+        "label": "Dario Kub",
+        "value": "Dario Kub"
+    },
+    {
+        "label": "Lorenz Fahey",
+        "value": "Lorenz Fahey"
+    },
+    {
+        "label": "Alicia Gutmann",
+        "value": "Alicia Gutmann"
+    },
+    {
+        "label": "Alexie Ryan",
+        "value": "Alexie Ryan"
+    },
+    {
+        "label": "Addison Aufderhar",
+        "value": "Addison Aufderhar"
+    },
+    {
+        "label": "Leila Kassulke",
+        "value": "Leila Kassulke"
+    },
+    {
+        "label": "Clemens Weissnat",
+        "value": "Clemens Weissnat"
+    },
+    {
+        "label": "Francisco Waters",
+        "value": "Francisco Waters"
+    },
+    {
+        "label": "Ruthie Kiehn",
+        "value": "Ruthie Kiehn"
+    },
+    {
+        "label": "Alba Hintz Jr.",
+        "value": "Alba Hintz Jr."
+    },
+    {
+        "label": "Merle Nicolas",
+        "value": "Merle Nicolas"
+    },
+    {
+        "label": "Hiram Kuvalis",
+        "value": "Hiram Kuvalis"
+    },
+    {
+        "label": "Audra Hodkiewicz",
+        "value": "Audra Hodkiewicz"
+    },
+    {
+        "label": "Adelle Parisian III",
+        "value": "Adelle Parisian III"
+    },
+    {
+        "label": "Sydni Mayert",
+        "value": "Sydni Mayert"
+    },
+    {
+        "label": "Monserrate Kilback",
+        "value": "Monserrate Kilback"
+    },
+    {
+        "label": "Elisabeth Bruen",
+        "value": "Elisabeth Bruen"
+    },
+    {
+        "label": "Samson Berge",
+        "value": "Samson Berge"
+    },
+    {
+        "label": "Lionel Gutmann",
+        "value": "Lionel Gutmann"
+    },
+    {
+        "label": "Rhiannon Streich",
+        "value": "Rhiannon Streich"
+    }
+]
