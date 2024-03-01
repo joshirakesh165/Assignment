ab52a4160e16496359235b1f3744d2078b9b47d0
joshirakesh165 <joshirakesh165@gmail.com>
2024-03-01 14:50:30 +0530

    Code pushed from Api

---
+++ src/Assignments/Demos/CodeViewerDemo2.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import CodeViewer from '../CodeViewer/CodeViewer'
+
+let code = `function CustomPromise(executor) {
+    let successFns = [];
+    let errorFns = [];
+    let state = 'PENDING';
+    let pValue;
+
+    function resolve(value) {
+        successFns.forEach(succ => succ(value));
+        pValue = value;
+        state = 'FULLFILLED'
+    }
+
+    function reject(error) {
+        errorFns.forEach(err => err(error))
+        pValue = err;
+        state = 'REJECTED'
+    }
+
+    executor(resolve, reject);
+    return {
+        then: (thenFn) => {
+            state == 'PENDING' ?  successFns.push(thenFn) : thenFn(pValue);
+            // can return new CustomPromise form here and mentained new variable to store next prmose chain
+        },
+        catch: (catchFn) => {
+            state == 'PENDING' ? errorFns.push(catchFn) :catchFn(pValue)
+        }
+    }
+
+}
+
+let cPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("abc")
+    }, 5000)
+})
+
+let f  = cPromise.then((res) => {
+    console.log('cp',res) // abc
+})
+`;
+
+const CodeViewerDemo2 = () => {
+    return (
+        <div>
+            <CodeViewer codeInStringForm={code} />
+        </div>
+    )
+}
+
+export default CodeViewerDemo2
