2a592196fc5c7accc42fae09e6ca8f2682eeeeaa
joshirakesh165 <joshirakesh165@gmail.com>
2024-03-01 14:51:01 +0530

    Code pushed from Api

---
+++ src/Assignments/InfiniteScroll/InfiniteScroll.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from 'react'
+import fetchData from './fetchData.js'
+import './Infinitescroll.css'
+
+let offset = 0;
+
+function InfiniteScroll() {
+    const [images, setImages] = useState([])
+    const lastElementRef = useRef()
+
+    const getImages = async (pageNo, count) => {
+        let data = await fetchData(pageNo, count);
+        setImages(state => [...state, ...data.photos]);
+        offset++;
+    }
+
+    useEffect(() => {
+        let observer = new IntersectionObserver((entries, observer) => {
+            entries?.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    getImages(offset, 10)
+                }
+            })
+        });
+        observer.observe(lastElementRef.current)
+    }, []);
+
+    return (
+        <>
+            <div className='img-container'>
+                {images.map((image, index) => <img key={index} src={image.url} alt="" />)}
+            </div>
+            <div id="lastElement" ref={lastElementRef}>
+                <div className='shimmer-loader-container'>
+                    <div className='shimmer-loader'></div>
+                    <div className='shimmer-loader'></div>
+                    <div className='shimmer-loader'></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default InfiniteScroll
